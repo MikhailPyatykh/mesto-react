edecddfb1f87765e56be1e1c9c00918119660f53
Mikhail Pyatykh <tryingstuff@yandex.ru>
2022-06-12 03:01:08 +0900

    feat: Final Step Fix, Unbowed, Unbent, Unbroken

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,97 +1,113 @@
-import { apiConfig } from './constants'
+import { apiConfig } from "./constants";
 
 class Api {
   constructor(config) {
-      this._config = config;
+    this._config = config;
   }
 
   _makeRequest(promise) {
-      return promise.then((res) => {
-          if (res.ok) {
-              return res.json();
-          }
+    return promise
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
       })
       .then((obj) => {
-          return obj;
-      })
+        return obj;
+      });
   }
 
   getUserData() {
-    return this._makeRequest(fetch(`${this._config.baseUrl}/users/me`, {
-        method: 'GET',
-        headers: this._config.headers
-    }));
-}
+    return this._makeRequest(
+      fetch(`${this._config.baseUrl}/users/me`, {
+        method: "GET",
+        headers: this._config.headers,
+      })
+    );
+  }
 
   getCards() {
-      return this._makeRequest(fetch(`${this._config.baseUrl}/cards`, {
-          method: 'GET',
-          headers: this._config.headers
-      }));
+    return this._makeRequest(
+      fetch(`${this._config.baseUrl}/cards`, {
+        method: "GET",
+        headers: this._config.headers,
+      })
+    );
   }
 
   patchProfileInfo(data) {
-      return this._makeRequest(fetch(`${this._config.baseUrl}/users/me`, {
-          method: 'PATCH',
-          headers: this._config.headers,
-          body: JSON.stringify({
-              name: data.nameInput,
-              about: data.occupationInput
-          })
-      }));
+    return this._makeRequest(
+      fetch(`${this._config.baseUrl}/users/me`, {
+        method: "PATCH",
+        headers: this._config.headers,
+        body: JSON.stringify({
+          name: data.nameInput,
+          about: data.occupationInput,
+        }),
+      })
+    );
   }
 
   postNewCard(data) {
-      return this._makeRequest(fetch(`${this._config.baseUrl}/cards`, {
-          method: 'POST',
-          headers: this._config.headers,
-          body: JSON.stringify({
-              name: data.newPlaceNameInput,
-              link: data.newPlaceLinkInput
-          })
-      }));
+    return this._makeRequest(
+      fetch(`${this._config.baseUrl}/cards`, {
+        method: "POST",
+        headers: this._config.headers,
+        body: JSON.stringify({
+          name: data.newPlaceNameInput,
+          link: data.newPlaceLinkInput,
+        }),
+      })
+    );
   }
 
   deleteCard(data) {
-      return this._makeRequest(fetch(`${this._config.baseUrl}/cards/${data}`, {
-          method: 'DELETE',
-          headers: this._config.headers,
-      }));
+    return this._makeRequest(
+      fetch(`${this._config.baseUrl}/cards/${data}`, {
+        method: "DELETE",
+        headers: this._config.headers,
+      })
+    );
   }
 
   patchAvatar(data) {
-    return this._makeRequest(fetch(`${this._config.baseUrl}/users/me/avatar`, {
-        method: 'PATCH',
+    return this._makeRequest(
+      fetch(`${this._config.baseUrl}/users/me/avatar`, {
+        method: "PATCH",
         headers: this._config.headers,
         body: JSON.stringify({
-            avatar: data.avatarLinkInput
-        })
-    }));
+          avatar: data.avatarLinkInput,
+        }),
+      })
+    );
   }
 
   putLike(data) {
-      return this._makeRequest(fetch(`${this._config.baseUrl}/cards/${data._id}/likes`, {
-          method: 'PUT',
-          headers: this._config.headers,
-      }));
+    return this._makeRequest(
+      fetch(`${this._config.baseUrl}/cards/${data._id}/likes`, {
+        method: "PUT",
+        headers: this._config.headers,
+      })
+    );
   }
 
   removeLike(data) {
-      return this._makeRequest(fetch(`${this._config.baseUrl}/cards/${data._id}/likes`, {
-          method: 'DELETE',
-          headers: this._config.headers,
-          body: JSON.stringify({
-              likes: data
-          })
-      }));
+    return this._makeRequest(
+      fetch(`${this._config.baseUrl}/cards/${data._id}/likes`, {
+        method: "DELETE",
+        headers: this._config.headers,
+        body: JSON.stringify({
+          likes: data,
+        }),
+      })
+    );
   }
 
   checkLikeID(array, data) {
-      const arrayLikes = array.likes;
-      return arrayLikes.find(element => element._id === data._id);
-  };
+    const arrayLikes = array.likes;
+    return arrayLikes.find((element) => element._id === data._id);
+  }
 }
-
 
 // Инициализируем класс Api
 const api = new Api(apiConfig);
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,45 +1,46 @@
 export const formsValidationConfig = {
-  formSelector: '.popup__inputs',
-  inputSelector: '.popup__input',
-  inputErrorClass: 'popup__input_type_error',
-  submitButtonSelector: '.popup__submit-btn',
-  inactiveButtonClass: 'popup__submit-btn_disabled',
-}
+  formSelector: ".popup__inputs",
+  inputSelector: ".popup__input",
+  inputErrorClass: "popup__input_type_error",
+  submitButtonSelector: ".popup__submit-btn",
+  inactiveButtonClass: "popup__submit-btn_disabled",
+};
 
 export const elementsSelectors = {
-  nameProfile: '.profile__info-fio',
-  occupationProfile: '.profile__info-occupation',
-  popupView: '.popup_view',
-  placesList: '.places__list',
-  popupEditProfile: '.popup_edit_profile',
-  popupAddPlace: '.popup_add_place',
-  popupDeleteCard: '.popup_delete-card',
-  popupPatchAvatar: '.popup_patch-avatar',
-  placeID: '.place__id',
-  avatarProfile: '.profile__avatar-image',
-}
+  nameProfile: ".profile__info-fio",
+  occupationProfile: ".profile__info-occupation",
+  popupView: ".popup_view",
+  placesList: ".places__list",
+  popupEditProfile: ".popup_edit_profile",
+  popupAddPlace: ".popup_add_place",
+  popupDeleteCard: ".popup_delete-card",
+  popupPatchAvatar: ".popup_patch-avatar",
+  placeID: ".place__id",
+  avatarProfile: ".profile__avatar-image",
+};
 
 export const apiConfig = {
-  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-38',
+  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-38",
   headers: {
-    authorization: 'da546cc6-febd-4e48-90b5-e55f89894793',
-    'Accept': 'application/json',
-    'Content-type': 'application/json; charset=utf-8'
-  }
-}
+    authorization: "da546cc6-febd-4e48-90b5-e55f89894793",
+    Accept: "application/json",
+    "Content-type": "application/json; charset=utf-8",
+  },
+};
 
-export const buttonEditProfile = document.querySelector('.profile__edit-button');
-export const buttonAddPlace = document.querySelector('.profile__add-button');
-export const buttonPatchAvatar = document.querySelector('.profile__avatar-button');
+export const buttonEditProfile = document.querySelector(".profile__edit-button");
+export const buttonAddPlace = document.querySelector(".profile__add-button");
+export const buttonPatchAvatar = document.querySelector(".profile__avatar-button");
 
 // export const avatarProfile = document.querySelector('.profile__avatar-image');
 // export const nameProfile = document.querySelector('.profile__info-fio');
-export const nameProfileInput = document.querySelector('.popup__input_type_fio');
-export const occupationProfile = document.querySelector('.profile__info-occupation');
-export const occupationProfileInput = document.querySelector('.popup__input_type_occupation');
+export const nameProfileInput = document.querySelector(".popup__input_type_fio");
+export const occupationProfile = document.querySelector(".profile__info-occupation");
+export const occupationProfileInput = document.querySelector(
+  ".popup__input_type_occupation"
+);
 
+export const placeNameInput = document.querySelector(".popup__input_type_place");
+export const placeLinkInput = document.querySelector(".popup__input_type_link");
 
-export const placeNameInput = document.querySelector('.popup__input_type_place');
-export const placeLinkInput = document.querySelector('.popup__input_type_link');
-
-export const cardTemplateSelector = '#place-template';
+export const cardTemplateSelector = "#place-template";
