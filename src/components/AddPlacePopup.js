273351a9ca32c4ac3fc4cb7b2f0d8b8ab7edc2fd
Mikhail Pyatykh <tryingstuff@yandex.ru>
2022-06-28 14:11:31 +0900

    feat: gh-pages

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState } from "react";
 import PopupWithForm from "./PopupWithForm";
 
 function AddPlacePopup(props) {
