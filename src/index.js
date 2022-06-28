1a4a816e282b5ef0bbb9b19f7a27333687b556c4
Mikhail Pyatykh <tryingstuff@yandex.ru>
2022-06-28 14:17:47 +0900

    feat: gh-pages

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./components/App";
-import reportWebVitals from "./reportWebVitals";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
-
 // import React from "react";
 // import ReactDOM from "react-dom/client";
-// import { HashRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 // import App from "./components/App";
 // import reportWebVitals from "./reportWebVitals";
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(
 //   <React.StrictMode>
-//     <HashRouter>
+//     <BrowserRouter>
 //       <App />
-//     </HashRouter>
+//     </BrowserRouter>
 //   </React.StrictMode>
 // );
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
